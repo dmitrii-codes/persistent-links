b9e6201063e5e5646933a142a77d18252b712f15
Jonathan Batty <jonathanbatty@gmail.com>
2021-09-18 16:19:59 +0100

    Updated styling, about and how it works pages

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import LinkBox from "../components/LinkBox";
 import NavigationBar from "../components/NavigationBar";
 import { NavBarPage } from "../components/NavigationBar";
-import { LinkSquare24Regular, Save24Regular} from "@fluentui/react-icons";
+
 
 const Home = () => {
     return (
         <div className="home">
             <NavigationBar activePage={NavBarPage.Home} />
             <div className="main_body">
-                <div className="link_input">
                     <LinkBox />
-                    <LinkSquare24Regular className="mr-3 icons"></LinkSquare24Regular>
-                </div>
+            </div>
+            <div className="footer">
+                PermaLink was created for the UoL BSc Computer Science course CM2020: Agile Software Projects, by Team 6, Tutor Group 2 (J Batty, S Dattatreya, C Ojiba, I Sheresh, D Vasilev). All rights reserved.
             </div>
         </div>
     );
--- src/views/HowItWorks.tsx
+++ src/views/HowItWorks.tsx
@@ -1,22 +1,62 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
 import { NavBarPage } from "../components/NavigationBar";
+import step1a from '../static/img/step1a.png';
+import step1b from '../static/img/step1b.png';
+import step2 from '../static/img/step2.png';
+import step3 from '../static/img/step3.png';
 
 const HowItWorks = () => {
     return (
         <div className="home">
             <NavigationBar activePage={NavBarPage.HowItWorks} />
-            <ul>
-                <li>Step 1. Submit the link you would like to preserve.</li>
-                <li>
-                    Step 2. Copy a new generated link from the confirmation
-                    page.
-                </li>
-                <li>
-                    Step 3. The generated link will display the current snapshot
-                    of the web page. Use it as needed.
-                </li>
-            </ul>
+            <div className="content">
+                <div>
+                    <h1 className="display-4">How to share a PermaLink</h1>
+                    <p className="lead">Share a permanent link in 3 easy steps...</p>
+                </div>
+                <div className="py-2">
+                    <p><b>Step 1:</b> Copy and paste the URL for the website into the input box on the PermaLink home page, and click "Generate PermaLink".</p>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col"><img className="tutorial" src={step1a}/></div>
+                            <div className="col"><img className="tutorial" src={step1b}/></div>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div className="py-2">
+                    <p><b>Step 2:</b> A permanent, unchangable URL is generated: this is the Permalink! Hit 'copy' to copy this to the clipboard, and share.</p>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col"><img className="tutorial" src={step2}/></div>
+                        </div>
+                    </div>
+                    
+                </div>
+                
+                
+                <div className="py-2">
+                    <p><b>Step 3:</b> When the PermaLink is accessed, it will retrieve the archived page, complete with the timestamp at which the link was archived.</p>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col"><img className="tutorial" src={step3}/></div>
+                        </div>
+                    </div>
+                    
+                </div>
+    
+            </div> 
+
+            <div className="container">
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+            </div>
+
+            <div className="footer">
+                PermaLink was created for the UoL BSc Computer Science course CM2020: Agile Software Projects, by Team 6, Tutor Group 2 (J Batty, S Dattatreya, C Ojiba, I Sheresh, D Vasilev). All rights reserved.
+            </div>
         </div>
     );
 };
