5ffca74e3244fc3ae52c7b6afe3293f15ef4c865
Dmitrii Vasilev <dmitrii.codes@gmail.com>
2021-09-18 21:37:07 -0700

    configured endpoint

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -1,4 +1,3 @@
 export const mainScrapperUrl = "***REMOVED***";
 export const scrapeHistoryUrl = "***REMOVED***";
-// export const origin = "http://persistentlinks.online";
-export const origin = "http://localhost:3000";
+export const origin = "http://persistentlinks.online";
